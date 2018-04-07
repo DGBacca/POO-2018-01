f2c063f571c0228c942691dae341c65d71aa16d3
JohnJAlpala <jjalpalac@unal.edu.co>
2018-04-07 18:17:34 -0500

    Update Admin.py

--- Admin.py
+++ Admin.py
@@ -1,8 +1,9 @@
-class Admin(object):
+import User from User
+class Admin(User):
 	"""docstring for Admin"""
-	def __init__(self, identificacion):
+	def __init__(self,identificacion,username,password,email):
+		super().__init__(username,email,password)
 		self._identificacion= identificacion
-
 
 
 	def enviarNotificacion(self):
